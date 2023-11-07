c36c6f33692086dd75ddedb4e281c70e45d8f24e
Prince-2004 <131356381+Prince-2004@users.noreply.github.com>
2023-11-07 12:52:22 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,61 +1,73 @@
-
-
+"""Calculator code"""
+import unittest
 class Calculator:
     """
     A simple calculator class that provides basic mathematical operations.
     """
-
    
-    def subtract(self, x, y):
+    def subtract(self, x_1, y_1):
         """
         Subtract two numbers.
-
-        :param x: The first number.
-        :param y: The second number.
-        :return: The result of subtracting y from x.
+        :param x_1: The first number.
+        :param y_1: The second number.
+        :return: The result of subtracting y_1 from x_1.
         """
-        return x - y
-    
-    def multiply(self, x, y):
+        return x_1 - y_1
+    def multiply_1(self, x_1, y_1):
         """
-        Multiply two numbers.
-
-        :param x: The first number.
-        :param y: The second number.
-        :return: The product of x and y.
+        Multiply_1 two numbers.
+        :param x_1: The first number.
+        :param y_1: The second number.
+        :return: The product of x_1 and y_1.
         """
-        return x * y
-    def divide(self, x, y):
+        return x_1 * y_1
+    def divide(self, x_1, y_1):
         """
         Divide two numbers.
-
-        :param x: The numerator.
-        :param y: The denominator.
-        :return: The result of dividing x by y.
-        :raises ValueError: If y is 0, a division by zero error is raised.
+        :param x_1: The numerator.
+        :param y_1: The denominator.
+        :return: The result of dividing x_1 by_1 y_1.
+        :raises ValueError: If y_1 is 0, a division by_1 zero error is raised.
         """
-        if y == 0:
-            raise ValueError("Division by zero is not allowed")
-        return x / y
+        if y_1 == 0:
+            raise ValueError("Division by_1 zero is not allowed")
+        return x_1 / y_1
+    def add1(self, x_1, y_1):
+        """
+        Add two numbers.
+        :param x_1: The first number.
+        :param y_1: The second number.
+        :return: The sum of x_1 and y_1.
+        """
+        return x_1 + y_1
 class CalculatorTest(unittest.TestCase):
     """
     A test case for the Calculator class.
     """
-
     def setUp(self):
         self.calc = Calculator()
-
-    def test_add(self):
-        self.assertEqual(11, self.calc.add(3, 8), "The addition is wrong")
-
+    def test_add1(self):
+        """
+        Test the add method.
+        """
+        self.assertEqual(12, self.calc.add(4, 8), "The addition is wrong")
     def test_subtract(self):
-        self.assertEqual(12, self.calc.subtract(15, 3), "Subtraction is wrong")
-
-    def test_multiply(self):
-        self.assertEqual(30, self.calc.multiply(5, 6), "Multiplication is wrong")
-
+        """
+        Test the subtract method.
+        """
+        self.assertEqual(11, self.calc.subtract(14, 3), "Subtraction is wrong")
+    def test_multiply_1(self):
+        """
+        Test the multiply method.
+        """
+        self.assertEqual(20, self.calc.multiply_1(4, 5), "Multiplication is wrong")
     
+    def test_add1(self):
+        """
+        Test the add method.
+        """
+        self.assertEqual(18, self.calc.add(10, 8), "The addition is wrong")
 if __name__ == '__main__':
     unittest.main()
-    print("Prince")
+print("Prince")
 
